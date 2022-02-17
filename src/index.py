bd3aeddb7c7cf2d4856913a37a456a7ae9759b07
JorgeGonzalezPascual <jorge_roller_98@hotmail.com>
2022-02-17 16:02:07 +0100

    Update

--- src/index.py
+++ src/index.py
@@ -1,10 +1,15 @@
+from multiprocessing.connection import Client
 import discord
 from discord.ext import commands
+import random              
+from random import randint 
+
+
+TOKEN = 'OTQzODI0NDg0NTEzNzUxMDYy.Yg4rDA.p60NNYyoKLvPZrXovh6yy5EIE-g'
 
 #Invite: https://discord.com/api/oauth2/authorize?client_id=943824484513751062&permissions=8&scope=bot
 
 PREFIX = '^'
-TOKEN = 'OTQzODI0NDg0NTEzNzUxMDYy.Yg4rDA.p60NNYyoKLvPZrXovh6yy5EIE-g'
 
 bot = commands.Bot(command_prefix=PREFIX,description="This is a helper bot")
 
@@ -14,7 +19,19 @@ bot = commands.Bot(command_prefix=PREFIX,description="This is a helper bot")
 async def ping(ctx):
     await ctx.send('pong')
 
-#EVENTS
+async def awl(ctx):
+    isExit = randint(0,1)
+    if isExit:
+        #Client.kick(member)
+        await ctx.send('Muerto')
+    else:
+        await ctx.send('Has tenido suerte')
 
+
+
+#EVENTS
+@bot.event
+async def on_guild_integrations_update():
+    print("Bot is Ready, lets go!")
     
 bot.run(TOKEN)
