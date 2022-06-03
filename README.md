8c4c8b4ff407b60f3994d5f8d74b5090e6b4d81c
JorgeGonzalezPascual <jorge.gonzalez@uib.es>
2022-06-03 20:39:33 +0200

    Upload

---
+++ README.md
@@ -0,0 +1,19 @@
+#Dependencies
+##Entorno virtual
+python -m venv .venv
+
+#Libreria 
+pip install py-cord
+pip install py-cord==2.0rc1
+
+##Documentacion
+https://docs.pycord.dev/en/master/index.html
+
+#Funcionalidades
+1. Ruleta rusa de expulsión del server
+2. Pedir roles a un/unos admins
+3. Votaciones
+4. Añadir una lista de miembros o un registro (like cineastas)
+5. Reformatorio/carcel
+6. Musica
+7. Creación canales temporales (like focus)
--- hykeos.py
+++ hykeos.py
@@ -1,38 +1,45 @@
-from multiprocessing.connection import Client
 import discord
-from discord.ext import commands
-import random              
+import random
 from random import randint 
 
-
 TOKEN = 'OTQzODI0NDg0NTEzNzUxMDYy.Yg4rDA.p60NNYyoKLvPZrXovh6yy5EIE-g'
+ID_GUIRIS= 718460119993548800
+ID_ADMIN= 588492819606405133
 
-#Invite: https://discord.com/api/oauth2/authorize?client_id=943824484513751062&permissions=8&scope=bot
-
-PREFIX = '^'
-
-bot = commands.Bot(command_prefix=PREFIX,description="This is a helper bot")
+bot = discord.Bot(debug_guilds=[ID_GUIRIS])
 
 #METHODS
-@bot.command()
-#ctx is a context
+@bot.slash_command(description='pong 🏓')
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.respond('pong')
 
-@bot.command()
-async def on_message(message):
-    isExit = randint(0,1)
-    if isExit:
-        #Client.kick(member)
-        await ctx.send('Muerto')
+@bot.slash_command(description='El bot te saluda.')
+async def hello(ctx):
+    await ctx.respond(f'Hi {ctx.author.mention}, I\'m {bot.user.name}!')
+
+@bot.slash_command(description='Prueba suerte para ser tu propio jefe.')
+async def rnd(ctx):
+    if randint(0,1):
+        await ctx.respond(f'{ctx.author.mention} Has muerto')
+        await ctx.guild.kick(ctx.author) 
     else:
-        await ctx.send('Has tenido suerte')
+        await ctx.respond(f'{ctx.author.mention} Has tenido suerte')
 
+@bot.slash_command(description='Para ver todos los roles')
+async def rols(ctx):
+        await ctx.respond(f'Pendiente...')
 
+@bot.slash_command(description='Pide un rol al admin.')
+async def pls_rol(ctx):
+        ID_CHANNEL = 982345257142325269
+        chan = bot.get_channel(ID_CHANNEL)
+        #await ctx.respond(f'{rol}!')
+        await chan.send(f'Hi')
+        
 
 #EVENTS
 @bot.event
-async def on_guild_integrations_update():
+async def on_ready():
     print("Bot is Ready, lets go!")
     
 bot.run(TOKEN)
